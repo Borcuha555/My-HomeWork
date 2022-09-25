80d98edc52092f0475caf1d61ecb4c8b69a23334
Vladislav <borcuhawrestler555@gmail.com>
2022-09-25 12:42:50 +0300

    Сделал задачу с днями недели

---
+++ CSharpProgect/Ex002.2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15: Напишите программу, которая принимает 
+//на вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+string namber = Console.ReadLine();
+
+if(namber.ToLower() == "6" || namber.ToLower() == "7")
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.Write("нет");
+    Console.WriteLine(namber);
+}
