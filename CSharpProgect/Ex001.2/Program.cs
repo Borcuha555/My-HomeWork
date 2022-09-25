4121401728b0558eff8d522bac1b1007cfcfcbd9
Vladislav <borcuhawrestler555@gmail.com>
2022-09-25 10:26:27 +0300

    Задание ко второму семинару

---
+++ CSharpProgect/Ex001.2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число.");
+   while(true)
+    {
+        string input = Console.ReadLine();
+        Console.WriteLine("{0}->{1}",input, input[1]);
+    }
+
+
+
