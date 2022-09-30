bf7cc3a761b1a55d01db5cfb7f32ff1cff4d7855
Vladislav <borcuhawrestler555@gmail.com>
2022-09-30 22:25:26 +0300

    Task 3

---
+++ CSharpProgect/Ex003.2/Ex003.1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите число и я возведу его в квадрат");
+int number = Convert.ToInt32(Console.ReadLine());
+int cube = number + 1;
+int index = 1;
+while(index < cube)
+{
+    int rezalt = index * index * index;
+    Console.WriteLine(rezalt);
+    index++;
+}
